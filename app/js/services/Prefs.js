5fd818d85c23ddfe24402aa97147469a5a583cf1
compact <adjklx@gmail.com>
2014-01-08 22:19:08 -0500

    turn Prefs into a provider

--- app/js/services/Prefs.js
+++ app/js/services/Prefs.js
@@ -1,29 +1,33 @@
-angular.module('app').factory('Prefs', function service() {
-  var defaultPrefs, Prefs;
-
-  defaultPrefs = {
-    'showFileDetails': false,
+angular.module('app').provider('Prefs', function () {
+  // configurable default prefs
+  this.defaults = {
+    'showFileDetails': true,
     'sort': '-date',
     'filesPerPage': 60
   };
 
-  // initialize prefs
-  try {
-    Prefs = JSON.parse(localStorage.getItem('prefs')) || {};
-  } catch (e) {
-    Prefs = {};
+  // service
+  this.$get = function service() {
+    var Prefs;
+
+    // initialize prefs based on localStorage and defaults
+    try {
+      Prefs = JSON.parse(localStorage.getItem('prefs')) || {};
+    } catch (e) {
+      Prefs = {};
+    }
+    Prefs = _.assign({}, this.defaults, Prefs);
+
+    Prefs.set = function (key, value) {
+      Prefs[key] = value;
+      localStorage.setItem('prefs', JSON.stringify(Prefs));
+    };
+
+    // for boolean prefs
+    Prefs.toggle = function (key) {
+      Prefs.set(key, !Prefs[key]);
+    };
+
+    return Prefs;
   }
-  Prefs = _.assign({}, defaultPrefs, Prefs);
-
-  Prefs.set = function (key, value) {
-    Prefs[key] = value;
-    localStorage.setItem('prefs', JSON.stringify(Prefs));
-  };
-
-  // for boolean prefs
-  Prefs.toggle = function (key) {
-    Prefs.set(key, !Prefs[key]);
-  };
-
-  return Prefs;
 });
